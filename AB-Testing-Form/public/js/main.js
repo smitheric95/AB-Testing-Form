9ca970dc21388683fe53959654d427da75804c45
Eric Smith <eric@smu.edu>
2018-03-21 16:43:24 -0500

    simple form submit

--- AB-Testing-Form/public/js/main.js
+++ AB-Testing-Form/public/js/main.js
@@ -1,5 +1,3 @@
 $(document).ready(function() {
-	$('body').click(function() {
-		console.log('hi');
-	});
+	console.log('hi');
 });
--- AB-Testing-Form/src/routes.php
+++ AB-Testing-Form/src/routes.php
@@ -5,10 +5,17 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->post('/postForm', function ($request, $response, $args) {
+    // Sample log message
+    $data = $request->getParsedBody();
+
+    return json_encode($data);
 });
