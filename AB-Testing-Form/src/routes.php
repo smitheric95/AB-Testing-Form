41778a72e6340d76235ef7c5bb11a10354dee265
Eric Smith <eric@smu.edu>
2018-03-25 18:54:48 -0500

    colored question numbers

--- AB-Testing-Form/src/routes.php
+++ AB-Testing-Form/src/routes.php
@@ -22,7 +22,7 @@ $app->post('/postForm', function ($request, $response, $args) {
     // get the data from the form
     $data = $request->getParsedBody();
 
-    var_dump($data);
+    echo "<pre>";var_dump($data);echo "</pre>";
 
     return $this->renderer->render($response, 'thankyou.phtml', $args);
 });
